d8c5b41b4bc37f5f708078eb666e91bb19630b6f
John Hurliman <jhurliman@jhurliman.org>
2013-07-03 15:44:47 -0700

    Misc. fixes and improvements

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var nconf = require('nconf');
 var log = require('winston');
 var express = require('express');
@@ -82,51 +83,73 @@ function postHook(req, res, next) {
   if (!payload.repository)
     return next('Unrecognized payload: ' + req.body.payload);
 
+  // Get the URL of the repository that this ping is about
   var repoUrl = (payload.canon_url) ?
     payload.canon_url + payload.repository.absolute_url :
     payload.repository.url;
   if (!repoUrl)
     return next('Unknown repository url in payload: ' + req.body.payload);
 
+  log.info('Received a ping for repository ' + repoUrl);
+
   // Forward this request
   var forwards = nconf.get('forward_to');
   if (forwards) {
     forwards.forEach(function(url) {
+      log.info('Forwarding ping to ' + url);
       request.post({ url: url, body: 'payload=' + req.body.payload }, function(err) {
         if (err)
-          log.warn('Failed to forward to ' + url + ': ' + err);
+          log.warn('Failed to forward ping to ' + url + ': ' + err);
+        else
+          log.info('Successfully forwarded ping to ' + url);
       });
     });
   }
 
+  // Get the list of configured repositories
   var repos = nconf.get('repositories');
   if (!repos)
     return res.send('OK');
 
-  repos.forEach(function(repo) {
-    if (repo.url === repoUrl)
-      updateRepo(repo);
-  });
+  // Find configured repositories that match the current repo ping
+  repos = repos.filter(function(repo) { return repo.url === repoUrl; });
+  if (!repos.length)
+    return res.send('OK');
+
+  // Update/deploy each configured repository matching the current ping
+  async.eachSeries(repos, updateRepo,
+    function(err) {
+      if (err)
+        log.error(err);
+      else
+        log.info('Finished updating all repositories');
+    }
+  );
+
+  res.send('OK');
 }
 
-function updateRepo(repo) {
+function updateRepo(repo, callback) {
   log.info('Updating repository ' + repo.path);
   exec('git pull', { cwd: repo.path, timeout: PULL_TIMEOUT_MS }, function(err, stdout, stderr) {
     if (err || stderr)
-      return log.error('git pull in ' + repo.path + ' failed: ' + (err || stderr));
+      return callback('git pull in ' + repo.path + ' failed: ' + (err || stderr));
 
     log.debug(stdout);
     log.info('Updated repository ' + repo.url + ' -> ' + repo.path);
 
-    if (repo.deploy) {
-      log.info('Running deployment ' + repo.deploy);
-      exec(repo.deploy, { cwd: repo.path, timeout: DEPLOY_TIMEOUT_MS }, function(err, stdout, stderr) {
-        if (err || stderr)
-          return log.error('deploy ' + repo.deploy + ' failed: ' + (err || stderr));
+    if (!repo.deploy)
+      return callback();
 
-        log.debug(stdout);
-        log.info('Finished deployment ' + repo.deploy);
-      });
-    }
+    log.info('Running deployment ' + repo.deploy);
+    exec(repo.deploy, { cwd: repo.path, timeout: DEPLOY_TIMEOUT_MS }, function(err, stdout, stderr) {
+      if (err || stderr)
+        return callback('Deploy ' + repo.deploy + ' failed: ' + (err || stderr));
+
+      log.debug(stdout);
+      log.info('Finished deployment ' + repo.deploy);
+
+      callback();
+    });
   });
 }
